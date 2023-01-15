c81c2f5fe72a7ae27d63d93fbd84f6fa49e6c6fa
PEDRO FERNANDO ALVAREZ SARMIENTO <pedro.alvarez.22@est.ucacue.edu.ec>
2023-01-15 13:13:20 -0500

    Cambion finales v1

--- PruebaAPI/Program.cs
+++ PruebaAPI/Program.cs
@@ -10,14 +10,38 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 app.UseSwagger();
 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwaggerUI();
 }
 
+
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("null"); // add the allowed origins  
+                      });
+});
+
+// services.AddResponseCaching();  
+
+builder.Services.AddControllers();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseCors(MyAllowSpecificOrigins);
